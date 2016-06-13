f9c6d7ae6d49b1918d478c4101b80d6febaa82c8
wolflion <successwolf@163.com>
2016-06-13 10:04:51 +0800

    update6月13
    更新 书的笔记以及书评，月季，粥记

---
+++ 读书笔记/2016/7月/管理思想史(6版).mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1465741375134" ID="ID_1389849130" MODIFIED="1465741386550" TEXT="&#x7ba1;&#x7406;&#x601d;&#x60f3;&#x53f2;(6&#x7248;)">
+<node CREATED="1465741410086" ID="ID_623401192" MODIFIED="1465741420966" POSITION="right" TEXT="Part1 &#x65e9;&#x671f;&#x7ba1;&#x7406;&#x601d;&#x60f3;"/>
+<node CREATED="1465741436031" ID="ID_341372038" MODIFIED="1465741443958" POSITION="right" TEXT="Part2 &#x79d1;&#x5b66;&#x7ba1;&#x7406;&#x65f6;&#x4ee3;"/>
+<node CREATED="1465741444229" ID="ID_1984518356" MODIFIED="1465741450651" POSITION="right" TEXT="Part3 &#x793e;&#x4f1a;&#x4eba;&#x65f6;&#x4ee3;"/>
+<node CREATED="1465741451143" ID="ID_1293837154" MODIFIED="1465741462057" POSITION="right" TEXT="Part4 &#x73b0;&#x4ee3;"/>
+</node>
+</map>
