a5f2c83caaa9f13f679f107a35e3e11aa9873e64
lionel Shen <successwolf@163.com>
2017-03-15 22:20:23 +0800

    20170315实验室电脑更校
    20170315实验室电脑更校

--- OneNote格式/CPP语言/CodeC++面向对象程序设计习题解析与上机指导/chap3.cpp
+++ OneNote格式/CPP语言/CodeC++面向对象程序设计习题解析与上机指导/chap3.cpp
@@ -144,3 +144,179 @@ void testdate()
 	indate.show();
 	//return 0;
 }
+
+/*
+ * 3-22：类example,定义一个两维对象数组，对它们进行初始化，并打印出来。
+ */
+class example
+{
+	int x;
+public:
+	example(int n)
+	{
+		x = n;
+	}
+	int get_x()
+	{
+		return x;
+	}
+};
+
+void testexample()
+{
+	example op[5][3] = {
+		1,2,3,
+		4,5,6,
+		7,8,9,
+		10,11,12,
+		13,14,15
+	};
+	int i;
+	for (i = 0; i < 5; i++)
+	{
+		cout << op[i][0].get_x()<<' ';
+		cout << op[i][1].get_x()<<' ';
+		cout << op[i][2].get_x()<<'\n';
+	}
+	cout << "\n";
+	// return 0;
+}
+
+/*
+ * 3-23：说明当构造函数具有一个以上的参数时，如何对数组对象进行初始化。
+ */
+class example23
+{
+	int x,y;
+	example23(int n, int m)
+	{
+		x = n;
+		y = m;
+	}
+	int get_x()
+	{
+		return x;
+	}
+	int get_y()
+	{
+		return y;
+	}
+};
+
+void testexample23()
+{
+	example23 op[3][2] = {   // 数组对象op通过直接调用构造函数声明，并初始化
+		example23(1,2),example23(3,4),
+		example23(5,6),example23(7,8),
+		example23(9,10),example23(11,12)
+	};
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		cout << op[i][0].get_x()<<' ';
+		cout << op[i][0].get_y()<<'\n';
+		cout << op[i][1].get_x()<<' ';
+		cout << op[i][1].get_y()<<'\n';
+	}
+	cout << "\n";
+	// return 0;
+}
+
+/*
+ * 3-24：类cube，num初始化为1-10，将cub初化为num的三次方值。
+ */
+class cube
+{
+	int num,cub;
+public:
+	cub(int a ,int b)
+	{
+		num = a; 
+		cub = b;
+	}
+	void show()
+	{
+		cout << num << ' ' << cub << endl;
+	}
+};
+
+void testcube()
+{
+	cube ob[10] = {
+		cube(1,1),
+		cube(2,8),
+		cube(3,27),
+		cube(4,64),
+		cube(5,125),
+		cube(6,216),
+		cube(7,343),
+		cube(8,512),
+		cube(9,729),
+		cube(10,1000)
+	};
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		ob[i].show();
+	}
+	// return 0;
+}
+
+/*
+ * 3-25：通过对象指针来访问对象数组，使程序以相反的顺序显示对象数组的内容。
+ */
+// class的定义，同example23
+void testexample25()
+{
+	example23 op[4] = {
+		example23(1,2),
+		example23(3,4),
+		example23(5,6),
+		example23(7,8)
+	};
+	int i;
+	example23 *p;
+	p = &op[3];  // 取出最后一个数组元素的地址
+	for(i=0;i<4;i++)
+	{
+		cout << p->get_x() << ' ';
+		cout << p->get_y() << "\n";
+		p--; // 指向前一个数组元素
+	}
+	cout << endl;
+}
+
+/*
+ * 3-27：分析程序
+ */
+class example27
+{
+	int i;
+public:
+	example27(int n)
+	{
+		i = n;
+		cout << "Constructing" << endl;
+	}
+	~example27()
+	{
+		cout << "Destructing" << endl;
+	}
+	int get_i()
+	{
+		return i;
+	}
+};
+
+int sqr_it(example27 o)
+{
+	return o.get_i() * o.get_i();
+}
+
+void testexample27()  // 调用一次构造函数，却调用了两次析构函数
+{
+	example27 x(10);
+	cout<<x.get_i()<<endl;
+	cout<<sqr_it(x)<<endl;
+	//return 0;
+}
---
+++ 实验室传承/106SVNReadMe.md
@@ -0,0 +1,19 @@
+##  这是关于106SVN目录的ReadMe
+
+##  Project List
++ SrimsVS
+	+ 科研管理信息系统可视化  by 李旺
++ Aspodes
+	+ 农科院项目申报系统  by 窦方坤
++ GraduationPrj
+	+ 本科毕业项目  by 马俊睿
++ Srims_Refactor
+	+ 科研管理信息系统重构  
++ Ipms
+	+ 985管理信息系统
++ SrimsYCS
+	+ 烟草所科技处成果模块以及重构版本 
+
+
+##  How to use
++ `http://222.195.149.106/svn/` + ProjectName
